60a1882fe4043245863c72deb72ef27c160f1ad4
Joseph Pereira <joepocalypse@hotmail.com>
2022-02-21 21:57:01 -0500

    Null check page data

--- pages/[[...page]].tsx
+++ pages/[[...page]].tsx
@@ -56,9 +56,9 @@ export default function Page({page}: InferGetStaticPropsType<typeof getStaticPro
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <meta name="description" content={page.data.description} />
+                <meta name="description" content={page?.data.description} />
 
-                <title>{page.data.title} | Bay State Girls Softball</title>
+                <title>{page?.data.title} | Bay State Girls Softball</title>
             </Head>
             <BuilderComponent model="page" content={page}/>
         </>
